9cce8b4a34cb7b64a3905aede793726f08758a01
pokemonnnn <93369245+pokemonnnn@users.noreply.github.com>
2024-11-15 08:50:43 +0700

    Pushing the base code

---
+++ client/src/main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/Login";
+
+const router = createBrowserRouter([
+    {
+        path: "/login",
+        element: <LoginPage />,
+    },
+    {
+        path: "/",
+        element: <div>Home</div>,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
